a78736b55522ef2f5482b77498f4531d0d05ba99
Zeinab-Jannati <Zaynab5@gmail.com>
2025-04-12 16:46:13 +0330

    API for Sign up page

--- back-end/project/config/config/settings.py
+++ back-end/project/config/config/settings.py
@@ -23,7 +23,9 @@ SIMPLE_JWT = {
 
 # Load environment variables from .env file
 load_dotenv()
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 CORS_ALLOW_HEADERS = ["authorization", "content-type"]
 
